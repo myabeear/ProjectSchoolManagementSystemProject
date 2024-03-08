e39ff7dd6766b8be365f29f2c6740830ca38e5a4
myabear <mbayuriandi@gmail.com>
2024-03-08 11:01:05 +0700

    first commit

---
+++ SchoolManagementSystem/Admin/ClassFees.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/AdminMst.Master" AutoEventWireup="true" CodeBehind="ClassFees.aspx.cs" Inherits="SchoolManagementSystem.Admin.ClassFees" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div style="background-image:url('../Image/bg4.jpg'); width:100%; height:720px; background-repeat: no-repeat; background-size:cover; background-attachment:fixed;">
+    <div class="container p-md-4 p-sm-4">
+        <div>
+            <asp:Label ID="lblMsg" runat="server"></asp:Label>
+        </div>
+        <h3 class="text-c">New Fees</h3>
+
+        <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-5">
+            <div class="col-md-6">
+                <label for="ddlClass">Class </label>
+                <asp:DropDownList ID="ddlClass" runat="server" CssClass="form-control"></asp:DropDownList>
+                <asp:RequiredFieldValidator runat="server" ErrorMessage="Class is required." ControlToValidate="ddlClass" Display="Dynamic" ForeColor="Red" InitialValue="Select Class" SetFocusOnError="True"></asp:RequiredFieldValidator>
+            </div>
+
+             <div class="col-md-6">
+            <label for="txtFeeAmounts">Fees(Annual)</label>
+              <asp:TextBox ID="txtFeeAmounts" runat="server" CssClass="form-control" placeholder="Enter Fees Amount" TextMode="Number" required></asp:TextBox>
+ </div>
+        </div>
+
+        <div class="row mb-3 mr-lg-5 ml-lg-5">
+             <div class="col-md-3 col-md-offset-2 mb-3">
+                 <asp:Button ID="bntAdd" runat="server" CssClass="btn btn-primary btn-block" BackColor="#5558C9" Text="Add Class" OnClick="bntAdd_Click" />
+             </div>
+        </div>
+
+        <div class="row mb-3 mr-lg-5 ml-lg-5">
+          <div class="col-md-8">
+              <asp:GridView ID="GridView1" runat="server" CssClass="table table-hover table-bordered" EmptyDataText="No Record To Display!" 
+                  AutoGenerateColumns="False" AllowPaging="true" PageSize="4" OnPageIndexChanging="GridView1_PageIndexChanging" DataKeyNames="FeesId" OnRowCancelingEdit="GridView1_RowCancelingEdit" 
+                  OnRowDeleting="GridView1_RowDeleting" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating">
+                  <Columns>
+                      <asp:BoundField DataField="No" HeaderText="No" ReadOnly="true">
+                      <ItemStyle HorizontalAlign="Center" />
+                      </asp:BoundField>
+                      <asp:BoundField DataField="ClassName" HeaderText="Class" ReadOnly="True">
+                      <ItemStyle HorizontalAlign="Center" />
+                      </asp:BoundField>
+                      <asp:TemplateField HeaderText="Fees(Annual)">
+                          <EditItemTemplate>
+                              <asp:TextBox ID="TextBox1" runat="server" Text='<%# Eval("FeesAmount") %>' CssClass="form-control"></asp:TextBox>
+                          </EditItemTemplate>
+                          <ItemTemplate>
+                              <asp:Label ID="Label1" runat="server" Text='<%# Eval("FeesAmount") %>'></asp:Label>
+                          </ItemTemplate>
+                          <ItemStyle HorizontalAlign="Center" />
+                      </asp:TemplateField>
+                      <asp:CommandField CausesValidation="false" HeaderText="Operation" ShowDeleteButton="true" ShowEditButton="True">
+                      <ItemStyle HorizontalAlign="Center" />
+                      </asp:CommandField>
+                  </Columns>
+                  <HeaderStyle BackColor="#5558C9" ForeColor="White" />
+              </asp:GridView>
+          </div>
+        </div>
+    </div>
+
+</div>
+
+   
+
+</asp:Content>
---
+++ SchoolManagementSystem/Admin/TeacherSubject.aspx.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static SchoolManagementSystem.Models.CommonFn;
+
+namespace SchoolManagementSystem.Admin
+{
+    public partial class TeacherSubject : System.Web.UI.Page
+    {
+        Commonfnx fn = new Commonfnx();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetClass();
+                GetTeacher();
+                GetTeacherSubject();
+            }
+        }
+
+        private void GetClass()
+        {
+            DataTable dt = fn.Fetch("Select * from Class");
+            ddlClass.DataSource = dt;
+            ddlClass.DataTextField = "ClassName";
+            ddlClass.DataValueField = "ClassId";
+            ddlClass.DataBind();
+            ddlClass.Items.Insert(0, "Select Class");
+        }
+
+        private void GetTeacher()
+        {
+            DataTable dt = fn.Fetch("Select * from Teacher");
+            ddlTeacher.DataSource = dt;
+            ddlTeacher.DataTextField = "Name";
+            ddlTeacher.DataValueField = "TeacherId";
+            ddlTeacher.DataBind();
+            ddlTeacher.Items.Insert(0, "Select Teacher");
+        }
+
+        private void GetTeacherSubject()
+        {
+            DataTable dt = fn.Fetch(@"SELECT ROW_NUMBER() OVER(ORDER BY (SELECT 1)) AS [No], ts.Id, ts.ClassId, c.ClassName, ts.SubjectId, s.SubjectName,
+                                    ts.TeacherId, t.Name 
+                              FROM TeacherSubject ts 
+                              INNER JOIN Class c ON ts.ClassId = c.ClassId 
+                              INNER JOIN Subject s ON ts.SubjectId = s.SubjectId 
+                              INNER JOIN Teacher t ON ts.TeacherId = t.TeacherId");
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void ddlClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string classId = ddlClass.SelectedValue;
+            DataTable dt = fn.Fetch("SELECT * FROM Subject WHERE ClassId = '" + classId + "' ");
+            ddlSubject.DataSource = dt;
+            ddlSubject.DataTextField = "SubjectName";
+            ddlSubject.DataValueField = "SubjectId";
+            ddlSubject.DataBind();
+            ddlSubject.Items.Insert(0, "Select Subject");
+        }
+
+
+        protected void bntAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string classId = ddlClass.SelectedValue;
+                string subjectId = ddlSubject.SelectedValue;
+                string teacherId = ddlTeacher.SelectedValue;
+                DataTable dt = fn.Fetch("Select * from TeacherSubject where ClassId = '" + classId + "'and SubjectId='" + subjectId+ "' or TeacherId='"+teacherId+"' ");
+                if (dt.Rows.Count == 0)
+                {
+                    string query = "Insert into TeacherSubject values('" + classId + "','" +subjectId + "','" +teacherId + "')";
+                    fn.Query(query);
+                    lblMsg.Text = "Inserted Successfully";
+                    lblMsg.CssClass = "alert alert-success";
+                    ddlClass.SelectedIndex = 0;
+                    ddlSubject.SelectedIndex = 0;
+                    ddlTeacher.SelectedIndex = 0;
+                    GetTeacherSubject();
+                }
+                else
+                {
+                    lblMsg.Text = "Entered <b>Teacher Subject</b> already exists";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "'); </script>");
+            }
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            GetTeacherSubject();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            GetTeacherSubject();
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+
+            try
+            {
+                int teacherSubjectId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+                fn.Query("Delete from TeacherSubject where Id = '" + teacherSubjectId + "'");
+                lblMsg.Text = "Teacher Subject Deleted Successfully";
+                lblMsg.CssClass = "alert alert-success";
+                GridView1.EditIndex = -1;
+                GetTeacherSubject();
+            }
+            catch (Exception ex)
+            {
+
+                Response.Write("<script>alert('" + ex.Message + "'); </script>");
+            }
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            GetTeacherSubject();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = GridView1.Rows[e.RowIndex];
+                int teacherSubjectId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+                string classId = ((DropDownList)GridView1.Rows[e.RowIndex].Cells[2].FindControl("ddlClassGv")).SelectedValue;
+                string subjectId = ((DropDownList)GridView1.Rows[e.RowIndex].Cells[2].FindControl("ddlSubjectGv")).SelectedValue;
+                string teacherId = ((DropDownList)GridView1.Rows[e.RowIndex].Cells[2].FindControl("ddlTeacherGv")).SelectedValue;             
+                fn.Query("Update TeacherSubject set ClassId ='" + classId + "', SubjectId='" + subjectId + "', TeacherId='" + teacherId + "' where Id = '" + teacherSubjectId + "' ");
+                lblMsg.Text = "Teacher Subject Updated Successfully";
+                lblMsg.CssClass = "alert alert-success";
+                GridView1.EditIndex = -1;
+                GetTeacherSubject();
+            }
+            catch (Exception ex)
+            {
+
+                Response.Write("<script>alert('" + ex.Message + "'); </script>");
+            }
+        }
+
+        protected void ddlClassGv_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DropDownList ddlClassSelected = (DropDownList)sender;
+            GridViewRow row = (GridViewRow)ddlClassSelected.NamingContainer;
+            if(row != null)
+            {
+                if ((row.RowState & DataControlRowState.Edit) > 0)
+                {
+                    DropDownList ddlSubjectGV = (DropDownList)row.FindControl("ddlSubjectGv");
+                    DataTable dt = fn.Fetch("Select * FROM Subject where ClassId = '"+ ddlClassSelected.SelectedValue +"'");
+                    ddlSubjectGV.DataSource = dt;
+                    ddlSubjectGV.DataTextField = "SubjectName";
+                    ddlSubjectGV.DataValueField = "SubjectId";
+                    ddlSubjectGV.DataBind();
+                }
+            }
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                if ((e.Row.RowState & DataControlRowState.Edit) > 0)
+                {
+                    DropDownList ddlClass = (DropDownList)e.Row.FindControl("ddlClassGv");
+                    DropDownList ddlSubject = (DropDownList)e.Row.FindControl("ddlSubjectGv");
+                    DataTable dt = fn.Fetch("Select * FROM Subject where ClassId = '" + ddlClass.SelectedValue + "'");
+                    ddlSubject.DataSource = dt;
+                    ddlSubject.DataTextField = "SubjectName";
+                    ddlSubject.DataValueField = "SubjectId";
+                    ddlSubject.DataBind();
+                    ddlSubject.Items.Insert(0, "Select Subject");
+                }
+            }
+        }
+    }
+}
