c8e8105c01b758f0fb506bba5bb49b4139ba9e99
myabear <mbayuriandi@gmail.com>
2024-03-12 10:11:56 +0700

    Penambahan Sub Menu Expense Details

---
+++ SchoolManagementSystem/Admin/ExpenseDetails.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static SchoolManagementSystem.Models.CommonFn;
+
+namespace SchoolManagementSystem.Admin
+{
+    public partial class ExpenseDetails : System.Web.UI.Page
+    {
+        Commonfnx fn = new Commonfnx();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetExpenseDetails();
+            }
+        }
+
+        private void GetExpenseDetails()
+        {
+            DataTable dt = fn.Fetch(@"SELECT ROW_NUMBER() OVER(ORDER BY (SELECT 1)) AS [No], e.ExpenseId, e.ClassId, c.ClassName, e.SubjectId, 
+                                        s.SubjectName, e.ChargeAmount from Expense e 
+                              INNER JOIN Class c ON e.ClassId = c.ClassId 
+                              INNER JOIN Subject s ON e.SubjectId = s.SubjectId");
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+}
