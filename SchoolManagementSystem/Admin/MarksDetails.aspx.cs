8ea17e1fba12dcfdcd309da3618d030f0ac7352d
myabear <mbayuriandi@gmail.com>
2024-03-15 10:00:13 +0700

    git push

--- SchoolManagementSystem/Admin/MarksDetails.aspx.cs
+++ SchoolManagementSystem/Admin/MarksDetails.aspx.cs
@@ -13,56 +13,11 @@ namespace SchoolManagementSystem.Admin
     
     public partial class MarksDetails : System.Web.UI.Page
     {
-        Commonfnx fn = new Commonfnx();
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                GetClass();
-                GetMarks();
-            }
+         
         }
 
-        private void GetMarks()
-        {
-            DataTable dt = fn.Fetch(@"SELECT ROW_NUMBER() OVER(ORDER BY(SELECT 1)) as [No], e.ExamId, e.ClassId, c.ClassName, e.SubjectId, s.SubjectName,
-                                    e.RollNo, e.TotalMarks, e.OutOfMarks from Exam e INNER JOIN Class c ON c.ClassId = e.ClassId INNER JOIN Subject s ON
-                                    s.SubjectId = e.SubjectId");
-            GridView1.DataSource = dt;
-            GridView1.DataBind();
-        }
 
-        private void GetClass()
-        {
-            DataTable dt = fn.Fetch("Select * from Class");
-            ddlClass.DataSource = dt;
-            ddlClass.DataTextField = "ClassName";
-            ddlClass.DataValueField = "ClassId";
-            ddlClass.DataBind();
-            ddlClass.Items.Insert(0, "Select Class");
-        }
-
-        protected void bntAdd_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                string classId = ddlClass.SelectedValue;
-                string rollNo = txtRoll.Text.Trim();
-                DataTable dt = fn.Fetch(@"SELECT ROW_NUMBER() OVER(ORDER BY(SELECT 1)) as [No], e.ExamId, e.ClassId, c.ClassName, e.SubjectId, s.SubjectName,
-                                    e.RollNo, e.TotalMarks, e.OutOfMarks from Exam e INNER JOIN Class c ON c.ClassId = e.ClassId INNER JOIN Subject s ON
-                                    s.SubjectId = e.SubjectId WHERE e.ClassId = '"+classId+"' and e.RollNo = '"+rollNo+"'");
-                GridView1.DataSource = dt;
-                GridView1.DataBind();
-            }
-            catch(Exception ex) 
-            {
-                throw;
-            }   
-        }
-
-        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
-        {
-            GridView1.PageIndex = e.NewPageIndex;
-        }
     }
 }
