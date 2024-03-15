4de43fef65effcac6ecc0083899c284e2d9ebe27
myabear <mbayuriandi@gmail.com>
2024-03-15 10:03:36 +0700

    Penambahan MarksDetailUserControl & Perbaikan Kode Program MarksDetails

--- SchoolManagementSystem/Teacher/MarksDetails.aspx.designer.cs
+++ SchoolManagementSystem/Teacher/MarksDetails.aspx.designer.cs
@@ -7,7 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SchoolManagementSystem.Admin
+namespace SchoolManagementSystem.Teacher
 {
 
 
@@ -15,12 +15,12 @@ namespace SchoolManagementSystem.Admin
     {
 
         /// <summary>
-        /// MarksDetail1 control.
+        /// MarksDetail control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::SchoolManagementSystem.MarksDetailUserControl MarksDetail1;
+        protected global::SchoolManagementSystem.MarksDetailUserControl MarksDetail;
     }
 }
