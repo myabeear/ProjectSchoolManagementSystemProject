9310158278ff84da920d5e199616e2bc2336c2ba
myabear <mbayuriandi@gmail.com>
2024-03-19 15:56:20 +0700

    Penambahan Tampilan Dashboard

--- SchoolManagementSystem/Admin/AdminHome.aspx.cs
+++ SchoolManagementSystem/Admin/AdminHome.aspx.cs
@@ -1,20 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using static SchoolManagementSystem.Models.CommonFn;
 
 namespace SchoolManagementSystem.Admin
 {
     public partial class AdminHome : System.Web.UI.Page
     {
+        // Objek fungsi umum untuk pengoperasian database
+        Commonfnx fn = new Commonfnx();
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["admin"] == null)
             {
                 Response.Redirect("../Login.aspx");
             }
+            else
+            {
+                StudentCount();
+                TeacherCount();
+                ClassCount();
+                SubjectCount();
+            }
+        }
+
+        void StudentCount()
+        {
+            DataTable dt = fn.Fetch("SELECT COUNT(*) from Student ");
+            Session["student"] = dt.Rows[0][0];
+        }
+
+        void TeacherCount()
+        {
+            DataTable dt = fn.Fetch("SELECT COUNT(*) from Student ");
+            Session["teacher"] = dt.Rows[0][0];
+        }
+
+        void ClassCount()
+        {
+            DataTable dt = fn.Fetch("SELECT COUNT(*) from Student ");
+            Session["class"] = dt.Rows[0][0];
+        }
+
+        void SubjectCount()
+        {
+            DataTable dt = fn.Fetch("SELECT COUNT(*) from Student ");
+            Session["subject"] = dt.Rows[0][0];
         }
     }
 }
